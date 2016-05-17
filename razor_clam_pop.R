e37d233acd4f656223a430767429e4f6ed76d26c
merrillrudd <merrillrudd@gmail.com>
2016-05-17 12:11:12 -0700

    updates to lefkovitz model

--- razor_clam_pop.R
+++ razor_clam_pop.R
@@ -7,212 +7,104 @@
 rm(list=ls()) ## clear environment
 setwd("C:\\Git_Projects\\razor_clam_pop") ## set working directory
 
+################ FUNCTIONS ###############################
+
+create_lefko <- function(M_eggs, M_prerecruits, M_recruits, F, fecundity, plus_yrs, prop_spawners){
+	lefko <- matrix(0, nrow=length(stages), ncol=length(stages))
+	rownames(lefko) <- colnames(lefko) <- stages
+	lefko["R","P"] <- tmat["R","P"] * exp(-M_prerecruits)
+	lefko["P","P"] <- tmat["P","P"] * exp(-M_prerecruits)
+	lefko["R","R"] <- tmat["R","R"] * exp(-M_recruits - F) * (1-exp(-M_recruits - F)^(plus_yrs - 1))/(1-(exp(-M_recruits - F)^plus_yrs))
+	lefko["P","R"] <- fecundity * exp(-M_eggs) * prop_spawners
+
+	return(lefko)
+}
+
+catch_fn <- function(abundance, F, M){
+	catch <- abundance * F * (1 - exp(-M-F) )/(M + F)
+	return(catch)
+}
+
+project_fn <- function(nyears, F_t, M_eggs, M_prerecruits, M_recruits){
+	pmat <- matrix(NA, nrow=nyears, ncol=4)
+	colnames(pmat) <- c(stages, "lambda", "catch")
+
+	lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, F=F_t[1], fecundity=clam_input$fecundity, plus_yrs=clam_input$plus_yrs, prop_spawners=clam_input$prop_spawners)
+	e1 <- eigen(lefko1)
+	pmat[1,stages] <- abs(e1$vectors[,1])
+	pmat[1,"lambda"] <- e1$value[1]
+	pmat[1,"catch"] <- catch_fn(abundance=pmat[1,"R"], F=F_t[1], M=M_recruits)
+
+	for(i in 2:nyears){	
+
+		lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, F=F_t[i], fecundity=clam_input$fecundity, plus_yrs=clam_input$plus_yrs, prop_spawners=clam_input$prop_spawners)
+		
+		pmat[i,stages] <- pmat[(i-1),stages] %*% t(lefko)
+		pmat[i,"lambda"] <- sum(pmat[i,stages])/sum(pmat[(i-1),stages])
+		pmat[i,"catch"] <- catch_fn(abundance=pmat[i,"R"], F=F_t[i], M=M_recruits)
+
+		rm(lefko)
+	}	
+
+	catch <- pmat[,"catch"]	
+
+	Outs <- NULL
+	Outs$pmat <- pmat
+	Outs$meancatch <- mean(catch)
+	Outs$totalcatch <- sum(catch)
+	return(Outs)
+}
+
 ################ LIFE HISTORY PARAMETERS #################
+
+clam_input <- list()
 
 ##### --------- Transition probabilities ------------  
 
-input <- list()
-input$LL <- 0 ## probability of larva staying larva
-input$SS <- 0 ## probability of set staying set
-input$JJ <- 0.2 ## probability of juvenile staying juvenile
-input$AA <- 1 ## probability of adult staying adult
-
-
-
-## --- probability of transitioning stages
-## larva to set
-LS <- 1
-
-## set to juvenile
-SJ <- 1
-
-## juvenile to adult
-JA <- 0.8
-
-## ---- probability of reproducing annually
-AL <- 1
-
-
-##### --------- Natural Survival probabilities -------
-
-## egg survival = function of ocean conditions
-survE <- 0.001
-
-## larva survival
-survL <- 0.05
-
-## set survival
-survS <- 0.1
-
-## juvenile survival
-survJ <- 0.2
-
-## adult survival
-survA <- 0.3
-
-##### ----------- Harvest rates ----------------------
-
-## harvest rate on juveniles
-hrJ <- 0.7
-
-## harvest rate on adults
-hrA <- 0.9
-
-##### ----------- Other parameters -------------------
-
-## fecundity (number of eggs produced by a single adult annually)
-fec <- 4e6
-
-## beach capacity = relative beach size (could increase above 1 from dredging or decrease from erosion or other factors)
-capacity <- 1
-
-## beach quality = relative quality (likely maximum at 1, would decrease below 1 due to driving on beach, runoff, horses, etc.)
-quality <- 1
-
-################ FUNCTIONS ###############################
-
-get_transitions <- function(input){
-    
-    stages <- c("L", "S", "J", "A")
-    tmat <- matrix(0, nrow=length(stages), ncol=length(stages))
-    rownames(tmat) <- colnames(tmat) <- stages
-    
-    #### ----------- from larva ---------------------------
-    tmat["L","L"] <- input$LL * input$survL * input$capacity * input$quality
-    
-    ## larva to set = probability of transitioning * larva survival * beach capacity metric * beach quality metric
-    tmat["S","L"] <- (1-input$LL) * input$survL * input$capacity * input$quality
-    
-    #### ----------- from set -----------------------------
-    tmat["S","S"] <- input$SS * input$survS * input$quality
-    
-    ## set to juvenile = probability of transitioning * set survival * beach quality metric
-    tmat["J","S"] <- (1-input$SS) * input$survS * input$quality
-    
-    #### ----------- from juvenile ------------------------
-    ## juvenile to juvenile = probability of staying juvenile * juvenile survival * beach quality metric * probability of not being harvested
-    tmat["J","J"] <- input$JJ * input$survJ * input$quality * (1 - input$hrJ)
-    
-    ## juvenile to adult = probability of transitioning * juvenile survival * beach quality metric * probability of not being harvested
-    tmat["A","J"] <- (1-input$JJ) * input$survJ * input$quality * (1 - input$hrJ)
-    
-    #### ----------- from adult ---------------------------
-    ## adult to adult = probability of staying adult * adult survival * beach quality metric * probability of not being harvested
-    tmat["A","A"] <- input$AA * input$survA * input$quality * (1 - input$hrA)
-    
-    ## eggs to larva = probability of an adult reproducing * fecundity * egg survival
-    tmat["L","A"] <- input$AL * input$fec * input$survE
-    
-    return(tmat)
-}
-
-get_abundances <- function(input, tmat, nyears, stochastic=FALSE, sd=0.6, seed=123){
-    
-    ## initial population size by stage in numbers
-    initial <- c("L"=input$fec*input$survE, "S"=input$fec*input$survE*input$survL, "J"=input$fec*input$survE*input$survL*input$survS, "A"=input$fec*input$survE*input$survL*input$survJ)
-    
-    ## population matrix
-    stages <- c("L", "S", "J", "A")
-    pmat <- matrix(NA, nrow=nyears, ncol=(length(stages)+1))
-    colnames(pmat) <- c(stages, "lamda")
-    
-    ## start population at initial values in first year
-    pmat[1,] <- c(initial, NA)
-    
-    set.seed(seed)
-    dev <- rnorm(nyears,0,sd)
-    
-    ## loop over years, starting with year 2
-    for(y in 2:nyears){
-      
-      ## larva = number of adults last year * number of surviving eggs
-      if(stochastic==TRUE) pmat[y, "L"] <- (pmat[y-1, "A"] * tmat["L", "A"] + pmat[y-1, "L"] * tmat["L", "L"])*exp(dev[y])
-      if(stochastic==FALSE) pmat[y, "L"] <- (pmat[y-1, "A"] * tmat["L", "A"])
-      
-      ## sets = number of larva last year * joint probability of being a set this year
-      pmat[y, "S"] <- (pmat[y-1, "L"] * tmat["S", "L"]) + (pmat[y-1, "S"] * tmat["S", "S"])
-      
-      ## juveniles = (number of sets last year * joint probability of being a juv this year if you were a set last year) + (number of juveniles last year * joint probability of being a juvenile this year if you were a juv last year)
-      pmat[y, "J"] <- (pmat[y-1, "S"] * tmat["J","S"]) + (pmat[y-1, "J"] * tmat["J", "J"])
-      
-      ## adults = (number of juveniles last year * joint probability of being an adult this year if you were a juv last year) + (number of adults last year * joint probability of being an adult this year if you were an adult last year)
-      pmat[y, "A"] <-  (pmat[y-1, "J"] * tmat["A", "J"]) + (pmat[y-1, "A"] * tmat["A", "A"])
-      
-      ## population growth rate
-      pmat[y, "lamda"] <- sum(pmat[y-1,1:length(stages)])/sum(pmat[y,1:length(stages)])
-      
-    }
-    
-    return(pmat)
-}
-
-
-
-################ TRANSITION MATRIX ##############################
-
-stages <- c("L", "S", "J", "A")
+stages <- c("P", "R") ## pre-recruits and recruits
 tmat <- matrix(0, nrow=length(stages), ncol=length(stages))
 rownames(tmat) <- colnames(tmat) <- stages
+tmat["P","P"] <- 0.05
+tmat["R","P"] <- (1-tmat["P","P"])
+tmat["R","R"] <- 1
 
-#### ----------- from larva ---------------------------
-## larva to set = probability of transitioning * larva survival * beach capacity metric * beach quality metric
-tmat["S","L"] <- LS * survL * capacity * quality
+clam_input$stages <- stages
+clam_input$transitions <- tmat
 
-#### ----------- from set -----------------------------
-## set to juvenile = probability of transitioning * set survival * beach quality metric
-tmat["J","S"] <- SJ * survS * quality
+##### --------- Mortality rates -----------------------
 
-#### ----------- from juvenile ------------------------
-## juvenile to juvenile = probability of staying juvenile * juvenile survival * beach quality metric * probability of not being harvested
-tmat["J","J"] <- JJ * survJ * quality * (1 - hrJ)
+clam_input$M_eggs <- 1
+clam_input$M_prerecruits <- 0.75
+clam_input$M_recruits <- 0.5 ## Reidinger paper on Razor clam growth and natural mortality
+clam_input$F_recruits <- 0
 
-## juvenile to adult = probability of transitioning * juvenile survival * beach quality metric * probability of not being harvested
-tmat["A","J"] <- JA * survJ * quality * (1 - hrJ)
+##### --------- Years in each stage -----------------------
 
-#### ----------- from adult ---------------------------
-## adult to adult = probability of staying adult * adult survival * beach quality metric * probability of not being harvested
-tmat["A","A"] <- AA * survA * quality * (1 - hrA)
+clam_input$plus_yrs <- 3
 
-## eggs to larva = probability of an adult reproducing * fecundity * egg survival
-tmat["L","A"] <- AL * fec * survE
+##### --------- Other factors ------------------------
 
+clam_input$prop_spawners <- 1 	 ## probability of adults reproducing
+clam_input$fecundity <- 3 ## 300,000 eggs estimated min from Seafood Watch report (max was 118 million)
 
+# capacity <- 1  ## metric for beach capacity
+# quality <- 1	 ## metric for beach quality
 
-################ ABUNDANCES OVER TIME ##############################
+################ EXPLORE POPULATION DYNAMICS #################
 
-## number of years to project
-nyears <- 300
+nyears <- 20
+F0 <- rep(0, nyears)
+F_low <- rep(0.2, nyears)
+F_med <- rep(0.7, nyears)
+F_high <- rep(2, nyears)
+F_alt <- c(rep(c(2, 0), (nyears/2)))
 
-## initial population size by stage in numbers
-initial <- c("L"=fec*survE, "S"=fec*survE*survL, "J"=fec*survE*survL*survS, "A"=fec*survE*survL*survJ)
+m0 <- project_fn(nyears=nyears, F_t=F0, M_eggs=clam_input$M_eggs, M_prerecruits=clam_input$M_prerecruits, M_recruits=clam_input$M_recruits)
 
-## population matrix
-pmat <- matrix(NA, nrow=nyears, ncol=(length(stages)+1))
-colnames(pmat) <- c(stages, "lamda")
+m1 <- project_fn(nyears=nyears, F_t=F_low, M_eggs=clam_input$M_eggs, M_prerecruits=clam_input$M_prerecruits, M_recruits=clam_input$M_recruits)
 
-## start population at initial values in first year
-pmat[1,] <- c(initial, NA)
+m2 <- project_fn(nyears=nyears, F_t=F_med, M_eggs=clam_input$M_eggs, M_prerecruits=clam_input$M_prerecruits, M_recruits=clam_input$M_recruits)
 
-## loop over years, starting with year 2
-for(y in 2:nyears){
+m3 <- project_fn(nyears=nyears, F_t=F_high, M_eggs=clam_input$M_eggs, M_prerecruits=clam_input$M_prerecruits, M_recruits=clam_input$M_recruits)
 
-	## larva = number of adults last year * number of surviving eggs
-	pmat[y, "L"] <- pmat[y-1, "A"] * tmat["L", "A"]
-
-	## sets = number of larva last year * joint probability of being a set this year
-	pmat[y, "S"] <- pmat[y-1, "L"] * tmat["S", "L"]
-
-	## juveniles = (number of sets last year * joint probability of being a juv this year if you were a set last year) + (number of juveniles last year * joint probability of being a juvenile this year if you were a juv last year)
-	pmat[y, "J"] <- (pmat[y-1, "S"] * tmat["J","S"]) + (pmat[y-1, "J"] * tmat["J", "J"])
-
-	## adults = (number of juveniles last year * joint probability of being an adult this year if you were a juv last year) + (number of adults last year * joint probability of being an adult this year if you were an adult last year)
-	pmat[y, "A"] <-  (pmat[y-1, "J"] * tmat["A", "J"]) + (pmat[y-1, "A"] * tmat["A", "A"])
-
-	## population growth rate
-	pmat[y, "lamda"] <- sum(pmat[y-1,1:length(stages)])/sum(pmat[y,1:length(stages)])
-
-}
-
-## adjust parameter values to get the equilibrium population growth rate to 1
-plot(pmat[,"lamda"], type="l")
-abline(h=1, col="red", lwd=2)
+m4 <- project_fn(nyears=nyears, F_t=F_alt, M_eggs=clam_input$M_eggs, M_prerecruits=clam_input$M_prerecruits, M_recruits=clam_input$M_recruits)
