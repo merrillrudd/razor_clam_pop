2e54a701cc466a489a1fced8d6e190fb6dfecdde
merrillrudd <merrillrudd@gmail.com>
2016-06-24 17:17:36 -0700

    razor clam shiny app

--- server.R
+++ server.R
@@ -11,61 +11,66 @@ shinyServer(function(input, output) {
   ################ FUNCTIONS ###############################
     
   ## create lefkovitch matrix based on inputs
-  create_lefko <- function(M_eggs, M_prerecruits, M_recruits, F, fecundity, plus_yrs, prop_spawners){
+  create_lefko <- function(M_eggs, M_prerecruits, M_recruits, u, fecundity, plus_yrs, prop_spawners){
     stages <- c("P", "R")
     lefko <- matrix(0, nrow=length(stages), ncol=length(stages))
     rownames(lefko) <- colnames(lefko) <- stages
     
     tmat <- build_transitions()
+    
     lefko["R","P"] <- tmat["R","P"] * exp(-M_prerecruits)
     lefko["P","P"] <- tmat["P","P"] * exp(-M_prerecruits)
-    lefko["R","R"] <- tmat["R","R"] * exp(-M_recruits - F) * (1-exp(-M_recruits - F)^(plus_yrs - 1))/(1-(exp(-M_recruits - F)^plus_yrs))
-    lefko["P","R"] <- fecundity * exp(-M_eggs) * prop_spawners
+    lefko["R","R"] <- tmat["R","R"] * ((exp(-M_recruits)*(1-u))^(plus_yrs-1))/(1-(exp(-M_recruits)*(1-u)))
+    lefko["P","R"] <- 1#fecundity #* exp(-M_eggs)# * prop_spawners
     
     return(lefko)
   }
-  
-  ## harvest based on inputs
-  catch_fn <- function(abundance, F, M){
-    catch <- abundance * F * (1 - exp(-M-F) )/(M + F)
-    return(catch)
-  }
     
   ## project population forward in time based on inputs
-  project_fn <- function(nyears, F_t, M_eggs, M_prerecruits, M_recruits, fecundity, plus_yrs, prop_spawners){
+  project_fn <- function(nyears, u_t, M_eggs, M_prerecruits, M_recruits, fecundity, plus_yrs, prop_spawners, rate_Mpre=FALSE, rate_Mrec=FALSE){
     stages <- c("P", "R")
-    pmat <- matrix(NA, nrow=nyears, ncol=4)
-    colnames(pmat) <- c(stages, "lambda", "catch")
+    pmat <- matrix(NA, nrow=nyears, ncol=5)
+    colnames(pmat) <- c(stages, "lambda", "catch", "exploit")
     
-    lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, F=F_t[1], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==FALSE & rate_Mrec==FALSE) lefko0 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, u=0, fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==TRUE & rate_Mrec==FALSE) lefko0 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[1], M_recruits=M_recruits, u=0, fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==FALSE & rate_Mrec==TRUE) lefko0 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits[1], u=0, fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==TRUE & rate_Mrec==TRUE) lefko0 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[1], M_recruits=M_recruits[1], u=0, fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    e0 <- eigen(lefko0)
+    unfishedR <- abs(e0$vectors[,1])[2]
+    
+    if(rate_Mpre==FALSE & rate_Mrec==FALSE) lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, u=u_t[1], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==TRUE & rate_Mrec==FALSE) lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[1], M_recruits=M_recruits, u=u_t[1], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==FALSE & rate_Mrec==TRUE) lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits[1], u=u_t[1], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    if(rate_Mpre==TRUE & rate_Mrec==TRUE) lefko1 <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[1], M_recruits=M_recruits[1], u=u_t[1], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
     e1 <- eigen(lefko1)
     pmat[1,stages] <- abs(e1$vectors[,1])
     pmat[1,"lambda"] <- e1$value[1]
-    pmat[1,"catch"] <- catch_fn(abundance=pmat[1,"R"], F=F_t[1], M=M_recruits)
+    pmat[1,"catch"] <- pmat[1,"R"]*u_t[1]
+    pmat[1,"exploit"] <- u_t[1]
     
     for(i in 2:nyears){	
-      
-      lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, F=F_t[i], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+    
+      if(rate_Mpre==FALSE & rate_Mrec==FALSE) lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits, u=u_t[i], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+      if(rate_Mpre==TRUE & rate_Mrec==FALSE) lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[i], M_recruits=M_recruits, u=u_t[i], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+      if(rate_Mpre==FALSE & rate_Mrec==TRUE) lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits[i], u=u_t[i], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
+      if(rate_Mpre==TRUE & rate_Mrec==TRUE) lefko <- create_lefko(M_eggs=M_eggs, M_prerecruits=M_prerecruits[i], M_recruits=M_recruits[i], u=u_t[i], fecundity=fecundity, plus_yrs=plus_yrs, prop_spawners=prop_spawners)
       
       pmat[i,stages] <- pmat[(i-1),stages] %*% t(lefko)
       pmat[i,"lambda"] <- sum(pmat[i,stages])/sum(pmat[(i-1),stages])
-      pmat[i,"catch"] <- catch_fn(abundance=pmat[i,"R"], F=F_t[i], M=M_recruits)
+      if(pmat[i,"R"]<unfishedR*0.1) u_t[i] <- 0
+      pmat[i,"exploit"] <- u_t[i]
+      pmat[i,"catch"] <- pmat[i,"R"]*u_t[i]
       
       rm(lefko)
     }	
-    
-    catch <- pmat[,"catch"]	
-    
-    Outs <- NULL
-    Outs$pmat <- pmat
-    Outs$meancatch <- mean(catch)
-    Outs$totalcatch <- sum(catch)
-    return(Outs)
+
+    return(pmat)
   }
     
   ## calculate relative values
-  relative <- function(x){
-    x2 <- x/max(x)
+  relative <- function(x, max){
+    x2 <- x/max
     return(x2)
   }
   
@@ -83,23 +88,206 @@ shinyServer(function(input, output) {
     return(tmat)
   }
   
-  build_results <- function(nyears=20, inc_waves, HABs, pollution, NIX, hypoxia, dec_habitat, M_eggs, M_prerecruits, M_recruits){
-    scenarios <- c("inc_waves", "HABs", "pollution", "NIX", "hypoxia", "dec_habitat")
-    choose_scenarios <- c(inc_waves, HABs, pollution, NIX, hypoxia, dec_habitat)
+  build_results <- function(scenarios){
     
-    F_med <- rep(0.7, nyears)
+    Outs <- list()
+    u_t <- rep(input$u_equil, input$nyears)
+    Outs[["BAU"]] <- project_fn(nyears=input$nyears, u_t=u_t, M_eggs=input$M_eggs, M_prerecruits=input$M_prerecruits, M_recruits=input$M_recruits, fecundity=input$fecundity, plus_yrs=input$plus_yrs, prop_spawners=input$prop_spawners)
     
-    if(inc_waves==TRUE) M_prerecruits <- M_prerecruits*1.3
-    out <- project_fn(nyears=nyears, F_t=F_med, M_eggs=M_eggs, M_prerecruits=M_prerecruits, M_recruits=M_recruits)
-    return(out$pmat)
+    scenarios_on <- scenarios[which(scenarios==TRUE)]
+    if(length(scenarios_on>1)){
+      
+      M_prerecruits_val_vec <- M_recruits_val_vec <- NULL
+      u_t_val_mat <- NULL
+      
+      for(i in 1:length(scenarios)){
+        if(names(scenarios[i]) %in% names(scenarios_on) == FALSE) next
+        if(names(scenarios[i])=="inc_waves" & scenarios[[i]]==TRUE){
+          ## scenario-specific rates
+          u_t_new=rep(input$u_equil, input$nyears)
+          M_eggs_new=input$M_eggs
+          M_prerecruits_new=input$M_prerecruits
+          M_recruits_new=input$M_recruits
+          fecundity_new=input$fecundity
+          plus_yrs_new=input$plus_yrs
+          prop_spawners_new=input$prop_spawners
+          
+          val <- ifelse(input$inc_waves_strength=="Low", 1.10, ifelse(input$inc_waves_strength=="High", 1.30, stop("invalid input for change in wave frequency strength")))
+          rates <- seq(1, val, length=input$nyears)
+          M_prerecruits_new <- M_prerecruits_new*rates
+          M_prerecruits_val_vec <- c(M_prerecruits_val_vec, mean(rates))
+          Outs[["inc_waves"]] <- project_fn(nyears=input$nyears, u_t=u_t_new, M_eggs=M_eggs_new, M_prerecruits=M_prerecruits_new, M_recruits=M_recruits_new, fecundity=fecundity_new, plus_yrs=plus_yrs_new, prop_spawners=prop_spawners_new, rate_Mpre=TRUE)
+        }
+        if(names(scenarios[i])=="pollution" & scenarios[[i]]==TRUE){
+          ## scenario-specific rates
+          u_t_new=rep(input$u_equil, input$nyears)
+          M_eggs_new=input$M_eggs
+          M_prerecruits_new=input$M_prerecruits
+          M_recruits_new=input$M_recruits
+          fecundity_new=input$fecundity
+          plus_yrs_new=input$plus_yrs
+          prop_spawners_new=input$prop_spawners
+          
+          val <- ifelse(input$pollution_strength=="Low", 1.10, ifelse(input$pollution_strength=="High", 1.30, stop("invalid input for change in wave frequency strength")))
+          M_prerecruits_new <- M_prerecruits_new*val
+          M_recruits_new <- M_recruits_new*val
+          M_prerecruits_val_vec <- c(M_prerecruits_val_vec, val)
+          M_recruits_val_vec <- c(M_recruits_val_vec, val)
+          Outs[["pollution"]] <- project_fn(nyears=input$nyears, u_t=u_t_new, M_eggs=M_eggs_new, M_prerecruits=M_prerecruits_new, M_recruits=M_recruits_new, fecundity=fecundity_new, plus_yrs=plus_yrs_new, prop_spawners=prop_spawners_new)
+        }
+        if(names(scenarios[i])=="dec_habitat" & scenarios[[i]]==TRUE){
+          ## scenario-specific rates
+          u_t_new=rep(input$u_equil, input$nyears)
+          M_eggs_new=input$M_eggs
+          M_prerecruits_new=input$M_prerecruits
+          M_recruits_new=input$M_recruits
+          fecundity_new=input$fecundity
+          plus_yrs_new=input$plus_yrs
+          prop_spawners_new=input$prop_spawners
+          
+          val <- ifelse(input$dec_habitat_strength=="Low", 1.05, ifelse(input$dec_habitat_strength=="High", 1.20, stop("invalid input for change in wave frequency strength")))
+          rates <- seq(1, val, length=input$nyears)
+          M_prerecruits_new <- M_prerecruits_new*rates
+          M_recruits_new <- M_recruits_new*rates
+          M_prerecruits_val_vec <- c(M_prerecruits_val_vec, mean(rates))
+          M_recruits_val_vec <- c(M_recruits_val_vec, mean(rates))
+          Outs[["dec_habitat"]] <- project_fn(nyears=input$nyears, u_t=u_t_new, M_eggs=M_eggs_new, M_prerecruits=M_prerecruits_new, M_recruits=M_recruits_new, fecundity=fecundity_new, plus_yrs=plus_yrs_new, prop_spawners=prop_spawners_new, rate_Mpre=TRUE, rate_Mrec=TRUE)
+        }
+        if(names(scenarios[i])=="HABs" & scenarios[[i]]==TRUE){
+          ## scenario-specific rates
+          u_t_new=rep(input$u_equil, input$nyears)
+          M_eggs_new=input$M_eggs
+          M_prerecruits_new=input$M_prerecruits
+          M_recruits_new=input$M_recruits
+          fecundity_new=input$fecundity
+          plus_yrs_new=input$plus_yrs
+          prop_spawners_new=input$prop_spawners
+          
+          val <- ifelse(input$HABs_strength=="Lower", input$u_equil/2, ifelse(input$HABs_strength=="Business As Usual", input$u_equil, ifelse(input$HABs_strength=="Higher", input$u_equil*1.5, stop("invalid input for change in HABs strength"))))
+          freq <- ifelse(input$HABs_freq=="High", 2, ifelse(input$HABs_freq=="Low", 5, stop("Invalid input for frequency of HABs closures")))
+          index_off <- seq(freq, input$nyears, by=freq)
+          index_on <- which(1:input$nyears %in% index_off==FALSE)
+          u_t_new[index_off] <- 0
+          u_t_new[index_on] <- val
+          u_t_val_mat <- rbind(u_t_val_mat, u_t_new)
+          Outs[["HABs"]] <- project_fn(nyears=input$nyears, u_t=u_t_new, M_eggs=M_eggs_new, M_prerecruits=M_prerecruits_new, M_recruits=M_recruits_new, fecundity=fecundity_new, plus_yrs=plus_yrs_new, prop_spawners=prop_spawners_new)
+        }
+      }
+      ## combined rates
+      if(all(is.null(u_t_val_mat))) u_t_all <- u_t
+      if(all(is.null(u_t_val_mat))==FALSE){
+        u_t_meds <- apply(u_t_val_mat, 2, function(x) median(x))
+        u_t_all <- u_t
+        index_min <- which(u_t_meds < u_t)
+        index_max <- which(u_t_meds > u_t)
+        if(length(index_min)>0) u_t_all[index_min] <- sapply(1:length(index_min), function(x) min(u_t_meds[index_min[x]], u_t[index_min[x]]))
+        if(length(index_max)>0) u_t_all[index_max] <- sapply(1:length(index_max), function(x) max(u_t_meds[index_max[x]], u_t[index_max[x]]))
+      }
+      
+      M_eggs_all=input$M_eggs
+      if(length(M_prerecruits_val_vec)==0) M_prerecruits_all <- input$M_prerecruits
+      if(length(M_prerecruits_val_vec)>0){ 
+        for(i in 1:length(M_prerecruits_val_vec)){
+          if(i==1) M_prerecruits_all=input$M_prerecruits*M_prerecruits_val_vec[i]
+          if(i>1) M_prerecruits_all <- M_prerecruits_all*M_prerecruits_val_vec[i]
+        }
+      }
+      if(length(M_recruits_val_vec)==0) M_recruits_all <- input$M_recruits
+      if(length(M_recruits_val_vec)>0){
+        for(i in 1:length(M_recruits_val_vec)){
+          if(i==1) M_recruits_all=input$M_recruits*M_recruits_val_vec[i]
+          if(i>1) M_recruits_all <- M_recruits_all*M_recruits_val_vec[i]
+        }
+      }
+      fecundity_all=input$fecundity
+      plus_yrs_all=input$plus_yrs
+      prop_spawners_all=input$prop_spawners
+      
+      Outs[["total"]] <- project_fn(nyears=input$nyears, u_t=u_t_all, M_eggs=M_eggs_all, M_prerecruits=M_prerecruits_all, M_recruits=M_recruits_all, fecundity=fecundity_all, plus_yrs=plus_yrs_all, prop_spawners=prop_spawners_all)
+    }
+    return(Outs)
   }
   
   output$CatchOverTime <- renderPlot({
     
-    res <- build_results(input$inc_waves, input$HABs, input$pollution, input$NIX, input$hypoxia, input$dec_habitat, input$M_eggs, input$M_prerecruits, input$M_recruits)
-    plot(res$catch, type-"l", col=1, lwd=2, ylim=c(-0.02, 0.5), xaxs="i", yaxs="i", main="Expected catch over time", xlab="Years into future", ylab="Catch")
-    
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    plot(relative(res$BAU[,"catch"], max(res$BAU[,"catch"], na.rm=TRUE)), type="l", col=gray(0.5), lwd=2, ylim=c(0, 10), xaxs="i", yaxs="i", main="Expected catch over time", xlab="Years into future", ylab="Relative Catch")
+    if(any(scenarios==TRUE)) points(relative(res$total[,"catch"], max(res$BAU[,"catch"], na.rm=TRUE)), pch=19, col=gray(0.2), cex=2, xpd=NA)
   })
+  
+  output$RecruitsOverTime <- renderPlot({
+    
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    plot(res$BAU[,"R"], type="l", col=gray(0.5), lwd=2, ylim=c(0, 10), xaxs="i", yaxs="i", main="Expected number of recruits over time", xlab="Years into future", ylab="Relative number of recruits")
+    if(any(scenarios==TRUE)) points(res$total[,"R"], pch=19, col=gray(0.2), cex=2, xpd=NA)
+  })
+
+  output$MeanCatch <- renderPlot({
+    colors <- brewer.pal(4, "Set1")
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    mean_catch <- relative(sapply(1:length(res), function(x) mean(res[[x]][,"catch"])), max=mean(res$BAU[,"catch"]))
+    res_names <- sapply(1:length(res), function(x) names(res[x]))
+    res_colors <- list("BAU"=gray(0.5))
+    scen_colors <- colors[which(names(scenarios) %in% res_names)]
+    names(scen_colors) <- names(scenarios)[which(names(scenarios)%in% res_names)]
+    res_colors <- c(res_colors, scen_colors, "total"=gray(0.2))
+    
+    
+    plot(mean_catch, type="h", lwd=10, col=unlist(res_colors), xlim=c(0, length(scenarios)+3), ylim=c(0,10), xaxt="n", xlab="Scenario", ylab="Mean Catch", xaxs="i", yaxs="i")
+    axis(1, at=1:length(mean_catch), labels=res_names)
+  })
+  
+  output$TotalCatch <- renderPlot({
+    colors <- brewer.pal(4, "Set1")
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    sum_catch <- relative(sapply(1:length(res), function(x) sum(res[[x]][,"catch"])), max=sum(res$BAU[,"catch"]))
+    res_names <- sapply(1:length(res), function(x) names(res[x]))
+    res_colors <- list("BAU"=gray(0.5))
+    scen_colors <- colors[which(names(scenarios) %in% res_names)]
+    names(scen_colors) <- names(scenarios)[which(names(scenarios)%in% res_names)]
+    res_colors <- c(res_colors, scen_colors, "total"=gray(0.2))
+    
+    
+    plot(sum_catch, type="h", lwd=10, col=unlist(res_colors), xlim=c(0, length(scenarios)+3), xpd=NA, ylim=c(0,10), xaxt="n", xlab="Scenario", ylab="Total Catch", xaxs="i", yaxs="i")
+    axis(1, at=1:length(sum_catch), labels=res_names)
+  })
+  
+  output$NoCatch <- renderPlot({
+    colors <- brewer.pal(4, "Set1")
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    no_catch <- sapply(1:length(res), function(x) length(which(res[[x]][,"exploit"]==0)))
+    res_names <- sapply(1:length(res), function(x) names(res[x]))
+    res_colors <- list("BAU"=gray(0.5))
+    scen_colors <- colors[which(names(scenarios) %in% res_names)]
+    names(scen_colors) <- names(scenarios)[which(names(scenarios)%in% res_names)]
+    res_colors <- c(res_colors, scen_colors, "total"=gray(0.2))
+    
+    
+    plot(no_catch, type="h", lwd=10, col=unlist(res_colors), xlim=c(0, length(scenarios)+3), xpd=NA, ylim=c(0,10), xaxt="n", xlab="Scenario", ylab="Number of years", xaxs="i", yaxs="i")
+    axis(1, at=1:length(no_catch), labels=res_names)
+  })
+  
+  output$NoCatchByMeanCatch <- renderPlot({
+    colors <- brewer.pal(4, "Set1")
+    scenarios=list("inc_waves"=input$inc_waves, "HABs"=input$HABs, "pollution"=input$pollution, "dec_habitat"=input$dec_habitat)
+    res <- build_results(scenarios=scenarios)
+    no_catch <- sapply(1:length(res), function(x) length(which(res[[x]][,"exploit"]==0)))
+    mean_catch <- relative(sapply(1:length(res), function(x) mean(res[[x]][,"catch"])), max=mean(res$BAU[,"catch"]))
+    res_names <- sapply(1:length(res), function(x) names(res[x]))
+    res_colors <- list("BAU"=gray(0.5))
+    scen_colors <- colors[which(names(scenarios) %in% res_names)]
+    names(scen_colors) <- names(scenarios)[which(names(scenarios)%in% res_names)]
+    res_colors <- c(res_colors, scen_colors, "total"=gray(0.2))
+    mean_catch[which(mean_catch > 3)] <- 3
+    
+    plot(mean_catch, no_catch, pch=19, cex=2, col=unlist(res_colors), xlim=c(0, 3), ylim=c(0, 20), xpd=NA, xlab="Mean Catch", ylab="Number of years without catch", main="Tradeoff in Mean Catch and Number of Years Without Harvest", xaxs="i", yaxs="i")
+  })
+
   
 
   
--- ui.R
+++ ui.R
@@ -6,38 +6,50 @@
 #
 
 library(shiny)
+require(RColorBrewer)
+colors <- brewer.pal(4, "Set1")
 
 shinyUI(fluidPage(
   titlePanel("Razor clam population dynamics under scenarios of change"),
   
-  column(2,
+  column(1,
       h4("Baseline natural mortality"),
       numericInput("M_eggs", "Eggs:", value=1),
-      numericInput("M_prerecruits", "Pre-recruits:", value=0.75),
-      numericInput("M_recruits", "Recruits:", value=0.5),
+      numericInput("M_prerecruits", "Pre-recruits:", value=0.79),
+      numericInput("M_recruits", "Recruits:", value=0.3),
       h4("Other population processes"),
       numericInput("plus_yrs", "Number of years as recruit:", value=3),
       numericInput("prop_spawners", "Proportion of recruits spawning:", value=1),
-      numericInput("fecundity", "Number of eggs per adult (Hundred thousands):", value=3)
+      h4("Management"),
+      numericInput("u_equil", "Baseline harvest rate:", value=0.3),
+      numericInput("nyears", "Number of years to project:", value=20)
     ),
-  column(10, 
-      sidebarLayout(
-        sidebarPanel(
+  column(2, 
           h4("Scenarios of change"),
-          checkboxInput("inc_waves", "Increasing wave heights and storm surges in the Pacific Northwest", FALSE),
-          checkboxInput("HABs", "Harmful algal blooms", FALSE),
-          checkboxInput("pollution", "Pollution/oil spills", FALSE),
-          checkboxInput("NIX", "NIX", FALSE),
-          checkboxInput("hypoxia", "Hypoxia", FALSE),
-          checkboxInput("dec_habitat", "Habitat destruction", FALSE)
+          checkboxInput("inc_waves", div(strong("Increasing wave heights and storm surges"), style=paste0("color:", colors[1]))),
+          conditionalPanel(
+            condition = "input.inc_waves==true",
+            radioButtons("inc_waves_strength", div(h5(em("Strength of increase:")), style=paste0("color:", colors[1])), choice=list("High", "Low"), selected="Low")
           ),
-        mainPanel(
-          plotOutput("CatchOverTime")
+          checkboxInput("HABs", div(strong("Harmful algal blooms"), style=paste0("color:", colors[2]))),
+          conditionalPanel(
+            condition = "input.HABs==true",
+            radioButtons("HABs_freq", div(h5(em("Closure frequency:")), style=paste0("color:", colors[2])), choices=list("High","Low"), selected="Low"),
+            radioButtons("HABs_strength", div(h5(em("Open season harvest rate:")), style=paste0("color:", colors[2])), choice=list("Higher", "Business As Usual", "Lower"), selected="Business As Usual")
+          ),
+          checkboxInput("pollution", div(strong("Pollution/oil spills"), style=paste0("color:", colors[3]))),
+          conditionalPanel(
+            condition = "input.pollution==true",
+            radioButtons("pollution_strength", div(h5(em("Intensity:")), style=paste0("color:", colors[3])), choice=list("High", "Low"), selected="Low")
+          ),
+          checkboxInput("dec_habitat", div(strong("Habitat destruction"), style=paste0("color:", colors[4]))),
+          conditionalPanel(
+            condition = "input.dec_habitat==true",
+            radioButtons("dec_habitat_strength", div(h5(em("Rate of decrease:")), style=paste0("color:", colors[4])), choice=list("High", "Low"), selected="Low")
           )
-        )
-         )
-    )
-  
-  
-
-)
+     ),
+     column(9,
+          column(6, plotOutput("NoCatchByMeanCatch"),plotOutput("CatchOverTime")),
+          column(6, plotOutput("TotalCatch"), plotOutput("RecruitsOverTime"))
+          )
+))  
